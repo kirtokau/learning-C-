2d3900c5b9bd00c153b2b63cfa23811e25da4e2b
kirtokau <sakura316694@163.com>
2022-03-11 11:21:53 +0800

    2022_3_10 web file

---
+++ 2022_3_10/l菱形继承问题/l菱形继承问题/l菱形继承问题.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+//动物类
+class Animal
+{
+public:
+	int m_Age;
+
+};
+//羊类
+//利用虚继承可以解决菱形继承问题
+//继承之前 加上关键字virtual变为虚继承
+//Animal类成为虚基类
+class Sheep :virtual public Animal {};
+//鸵类
+class Tuo :virtual public Animal {};
+//羊驼类
+class SheepTuo :public Sheep, public Tuo {};
+void test01()
+{
+	SheepTuo st;
+	//利用虚继承可以解决菱形继承问题
+	st.Sheep::m_Age = 18;
+	st.Tuo::m_Age = 28;
+	//当菱形继承，两个父类拥有相同的数据，需要加以作用域区分 
+	cout << "st.sheep::m_Age=" << st.Sheep::m_Age << endl;
+	cout << "st.Tuo::m_Age=" << st.Tuo::m_Age << endl;
+	cout << "st.m_Age=" << st.m_Age << endl;
+	//这份数据我们知道只要有一份就可以了，菱形继承导致数据有两份，资源浪费
+
+}
+int main()
+{
+	test01();
+}
