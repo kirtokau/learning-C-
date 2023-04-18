7097957389d4ed00f3bcfe0c06fef150dd42464a
lv_hangyu <sakura316694@163.com>
2023-04-19 02:26:32 +0800

    2023_4_19

---
+++ 2023_4_19/Project1/分文件编写.cpp
@@ -0,0 +1,11 @@
+#include"swap.h"
+//int main(){
+//	int a = 10, b = 20;
+//	swap(a, b);
+//
+//	cout << "a=" <<a << endl;
+//	cout << "b=" <<b << endl;
+//	system("pause");
+//	return 0;
+//
+//}
---
+++ 2023_4_19/Project1/学生类.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#include<string>
+class Student
+{
+public:
+	void setName(string name)
+	{
+		m_name = name;
+	}
+
+	void setID(int id)
+	{
+		m_id = id;
+	}
+	void showStudent()
+	{
+		cout << "name:" << m_name << " ID:" << m_id << endl;
+	}
+public:
+	string m_name;
+	int m_id;
+};
+int main()
+{
+	Student stu;
+	stu.setName("Â³ÖÇÉî");
+	stu.setID(10);
+	stu.showStudent();
+
+	system("pause");
+	return 0;
+
+}
